d0a65b14c15b4d2ec1fc0df5651202f1ab8106c0
DaIsimsiz <daisimsiz.tr@gmail.com>
2026-03-13 19:46:31 +0300

    Adds a template treeview

--- planetDefs-Builder/scripts.js
+++ planetDefs-Builder/scripts.js
@@ -1,1 +1,9 @@
-﻿
+﻿var toggler = document.getElementsByClassName("expand");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".collapsed").classList.toggle("expanded");
+    this.classList.toggle("collapse");
+  });
+}
