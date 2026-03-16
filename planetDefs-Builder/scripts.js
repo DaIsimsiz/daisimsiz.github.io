e6ce1e706b0d033627d94664b10c0fbe670befb1
DaIsimsiz <daisimsiz.tr@gmail.com>
2026-03-16 13:21:46 +0300

    Cleanup and tip swapping

--- planetDefs-Builder/scripts.js
+++ planetDefs-Builder/scripts.js
@@ -1,4 +1,5 @@
-﻿var toggler = document.getElementsByClassName("expand");
+﻿// #region Old Code
+var toggler = document.getElementsByClassName("expand");
 var i;
 
 for (i = 0; i < toggler.length; i++) {
@@ -7,3 +8,36 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("collapse");
   });
 }
+// #endregion
+
+// #region TreeView
+var treeView = document.getElementById("galaxyTreeView");
+treeView.innerHTML += "<li id=\"galaxy\"><span class=\"icon\"></span>Galaxy</span></li>";
+var galaxy = document.getElementById("galaxy");
+// #endregion
+
+// #region Tips
+var tipButton = document.getElementById("tipButton")
+var tip = document.getElementById("tip")
+
+var tips = [
+  "Click on the gear icon to my left to see more tips :)",
+  "Right click on an element to see if you can add properties to it.",
+  "Double check your Advanced Rocketry configuration to ensure the mod uses your custom planetDefs.xml."
+  // TODO: Add more
+]
+
+tip.innerHTML = tips[0];
+tipButton.onclick = function() {
+  // TODO: Add animation
+  tip.innerHTML = tips[Math.floor(Math.random() * tips.length)];
+}
+// #endregion
+
+// #region Custom Context Menu
+var contextMenu = document.getElementsByClassName("contextMenu")[0];
+galaxy.addEventListener("contextmenu", function(evt) {
+  evt.preventDefault();
+  // Open CCM
+})
+// #endregion
